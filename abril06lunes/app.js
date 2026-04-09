e4125a4cb3d391fe4feb55d1f7ac9b53df06a26c
ianmadariaga07 <iantranquis11@gmail.com>
2026-04-09 00:18:03 -0600

    feat(tareas): uso de eventos con estructura de control para el DOM

---
+++ abril06lunes/app.js
@@ -0,0 +1,24 @@
+// console.log(document.titulo);
+// console.log(document.getElementById("titulo"))
+// console.log(document.getElementById("titulo").textContent)
+
+// console.log(document.querySelectorAll("h1"))
+// console.log(document.)
+
+let titulo1 = document.querySelector("#tituloID1");
+let titulo2 = document.querySelector("#tituloID2");
+let titulo3 = document.querySelector("#tituloID3");
+
+console.log(titulo1.textContent)
+console.log(titulo2.textContent)
+console.log(titulo3.textContent)
+
+titulo1.textContent = "Nuevo titulo 1";
+titulo2.textContent = "Nuevo titulo 2";
+titulo3.textContent = "Nuevo titulo 3";
+
+const h1 = document.querySelector('#titulo.')
+
+
+
+
